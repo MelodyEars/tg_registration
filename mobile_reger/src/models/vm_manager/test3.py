b552c5c2d48e7b72ddd9bd35c9ef436c0f5598d0
SimSams <danya221299@gmail.com>
2023-08-28 09:26:18 -0700

    vm manager

---
+++ mobile_reger/src/models/vm_manager/test3.py
@@ -0,0 +1,57 @@
+import contextlib
+
+from loguru import logger
+
+from mobile_reger.src.models.vm_manager.pyavd import pyavd
+
+# emulator_path = r'C:\Users\King\AppData\Local\Android\Sdk\tools\emulator.exe'
+# avdmanager = r'C:\Users\King\AppData\Local\Android\Sdk\cmdline-tools\latest\bin\avdmanager.bat'
+# api_32 = r'C:\Users\King\AppData\Local\Android\Sdk\system-images\android-32\google_apis\x86_64'
+# skin = r'C:\Users\King\AppData\Local\Android\Sdk\skins\pixel_6_pro'
+proxy = '-http-proxy ' + 'username:password@server:port'
+port = '5560'  # від 5554 до 5682
+memory = '2048'
+timezone = 'Europe/Paris'
+no_window = '-no-window'
+
+# example: '/Users/janedoe/Library/Android/sdk/emulator/emulator -avd Nexus_5X_API_23 -netdelay none -netspeed full'
+# my_cmd = f'''{emulator_path} -avd  TEST_VM_API_32 -no-snapshot-save -memory {memory} -debug all -show-kernel
+#              -http-proxy {proxy} -netdelay none -netspeed full -port {port} -accel auto --timezone {timezone}
+#               -no-boot-anim -screen multi-touch '''
+
+# avdmanager = r'C:\Users\King\AppData\Local\Android\Sdk\cmdline-tools\latest\bin\avdmanager.bat'
+# name_avd = 'TEST_A'
+# cmd = {'create': f'{avdmanager} create avd -n {name_avd} -k "system-images;android-32;google_apis;x86_64"',
+#        'delete': f'{avdmanager} delete avd -n {name_avd}'}
+#
+# delete_cmd = f'{avdmanager} delete avd -n {name_avd}'
+
+
+@contextlib.contextmanager
+def vm_manager(name_avd, proxy=None):
+	custom_config = f' -no-snapshot-save -memory {memory} -netdelay none -netspeed full -port {port} -accel auto -timezone {timezone} -no-boot-anim -screen multi-touch'
+	if proxy:
+		custom_config = custom_config + f" -http-proxy {proxy}"
+
+	create_param = {
+		'name': name_avd,
+		'device': pyavd.get_devices()[32],  # Pixel 6 Pro
+		'package': "system-images;android-32;google_apis;x86_64",
+		# 'skin': skin,
+		'force': True,
+	}
+
+	logger.debug("creating avd")
+	avd = pyavd.create_avd(**create_param)
+
+	logger.debug("starting avd")
+	p = avd.start(detach=True, config=custom_config)
+	logger.debug("waiting for avd")
+	try:
+		yield port
+	finally:
+		logger.debug("stopping avd")
+		avd.stop(port=port)
+		logger.debug("deleting avd")
+		avd.delete()
+		logger.debug("done")
