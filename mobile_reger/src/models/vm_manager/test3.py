13268c8ae191ab969866f71b8f67072aaa1062fd
SimSams <danya221299@gmail.com>
2023-08-28 11:00:11 -0700

    fix delete method vm manager

--- mobile_reger/src/models/vm_manager/test3.py
+++ mobile_reger/src/models/vm_manager/test3.py
@@ -1,4 +1,6 @@
 import contextlib
+import re
+import subprocess
 
 from loguru import logger
 
@@ -9,10 +11,10 @@ from mobile_reger.src.models.vm_manager.pyavd import pyavd
 # api_32 = r'C:\Users\King\AppData\Local\Android\Sdk\system-images\android-32\google_apis\x86_64'
 # skin = r'C:\Users\King\AppData\Local\Android\Sdk\skins\pixel_6_pro'
 proxy = '-http-proxy ' + 'username:password@server:port'
-port = '5560'  # від 5554 до 5682
-memory = '2048'
-timezone = 'Europe/Paris'
-no_window = '-no-window'
+
+
+
+
 
 # example: '/Users/janedoe/Library/Android/sdk/emulator/emulator -avd Nexus_5X_API_23 -netdelay none -netspeed full'
 # my_cmd = f'''{emulator_path} -avd  TEST_VM_API_32 -no-snapshot-save -memory {memory} -debug all -show-kernel
@@ -23,15 +25,31 @@ no_window = '-no-window'
 # name_avd = 'TEST_A'
 # cmd = {'create': f'{avdmanager} create avd -n {name_avd} -k "system-images;android-32;google_apis;x86_64"',
 #        'delete': f'{avdmanager} delete avd -n {name_avd}'}
-#
+
 # delete_cmd = f'{avdmanager} delete avd -n {name_avd}'
+
+memory = '2048'
+no_window = False  # a window appeared
 
 
 @contextlib.contextmanager
-def vm_manager(name_avd, proxy=None):
-	custom_config = f' -no-snapshot-save -memory {memory} -netdelay none -netspeed full -port {port} -accel auto -timezone {timezone} -no-boot-anim -screen multi-touch'
-	if proxy:
-		custom_config = custom_config + f" -http-proxy {proxy}"
+def vm_manager(name_avd, vm_proxy=None, timezone='Europe/Paris', vm_port='5554'):
+	custom_config = (
+		' -no-snapshot-save'
+		f' -memory {memory}'
+		' -netdelay none'
+		' -netspeed full'
+		f' -port {vm_port}'  # від 5554 до 5682
+		' -accel auto'
+		f' -timezone {timezone}'
+		' -no-boot-anim'
+		' -screen multi-touch'
+	)
+	if vm_proxy:
+		custom_config += f" -http-proxy {vm_proxy}"
+
+	if no_window:
+		custom_config += ' -no-window'
 
 	create_param = {
 		'name': name_avd,
@@ -48,10 +66,18 @@ def vm_manager(name_avd, proxy=None):
 	p = avd.start(detach=True, config=custom_config)
 	logger.debug("waiting for avd")
 	try:
-		yield port
+		yield vm_port
 	finally:
 		logger.debug("stopping avd")
-		avd.stop(port=port)
+		avd.stop(port=vm_port)
 		logger.debug("deleting avd")
 		avd.delete()
 		logger.debug("done")
+
+
+if __name__ == '__main__':
+	with vm_manager('TEST_A') as port:
+		print(port)
+		input("Press Enter to exit")
+
+
